cf0c192b215a1b9ae77ff8aa358f5b5b7910fad1
dp <dpronin@yandex.ru>
2019-07-15 23:23:10 -0700

    charts works

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
-import 'dart:math';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_circular_chart/flutter_circular_chart.dart';
 import 'package:toodoo/models/store_model.dart';
 import 'package:toodoo/widgets/edit_dialog_widget.dart';
 import 'package:toodoo/widgets/todo_widget.dart';
@@ -17,6 +18,9 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   CollectionReference collection = Firestore.instance.collection('todos');
+  final GlobalKey<AnimatedCircularChartState> _chartKey =
+      new GlobalKey<AnimatedCircularChartState>();
+  var _documents = <DocumentSnapshot>[];
 
   void _addTodoItem() {
     DocumentReference doc = collection.document();
@@ -28,70 +32,100 @@ class _MainPageState extends State<MainPage> {
         merge: true);
   }
 
+  _ipdateDocuments(List<DocumentSnapshot> documents) {
+    setState(() {
+      _documents = documents;
+    });
+  }
+
   Future _removeTodoItem(DocumentSnapshot document) async {
     await collection.document(document.documentID).delete();
   }
 
   Future _updateTodo(DocumentSnapshot document, dynamic x) {
-    collection.document(document.documentID).updateData(
+    return collection.document(document.documentID).updateData(
         {'priority': x['priority'].index, 'title': x['title'] = x['title']});
   }
 
   @override
   Widget build(BuildContext context) {
+    final stream = collection.orderBy('priority', descending: true).snapshots();
     return Scaffold(
         body: SafeArea(
-            child: CustomScrollView(
-          slivers: <Widget>[
-            TopWidget(),
-            StreamBuilder<QuerySnapshot>(
-              stream:
-                  collection.orderBy('priority', descending: true).snapshots(),
+          child: StreamBuilder<QuerySnapshot>(
+              stream: stream,
               builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                return SliverList(
-                  delegate: SliverChildBuilderDelegate((context, index) {
-                    final todo = Store.fromSnapshot(
-                              snapshot.data.documents[index]);
-                    return GestureDetector(
-                        child: Slidable(
-                          actionPane: SlidableDrawerActionPane(),
-                          actionExtentRatio: 0.25,
-                          child: TodoWidget(todo),
-                          actions: <Widget>[
-                            IconSlideAction(
-                              caption: 'Share',
-                              color: Colors.indigo,
-                              icon: Icons.share,
+                var priorities = groupBy(snapshot.data.documents, (x) => x['priority']);
+                if (!priorities.containsKey(0)) priorities[0] = [];
+                if (!priorities.containsKey(1)) priorities[1] = [];
+                if (!priorities.containsKey(2)) priorities[2] = [];
+                final totalLength = priorities.values.fold(0, (x, y) => 1.0 * x + y.length);
+                final low = priorities[0].length * 100.0 / totalLength;
+                final medium = priorities[1].length * 100.0 / totalLength;
+                final high = priorities[2].length * 100.0  / totalLength;
+                List<CircularStackEntry> nextData = <CircularStackEntry>[
+                  new CircularStackEntry(
+                    <CircularSegmentEntry>[
+                      new CircularSegmentEntry(low, Colors.greenAccent.shade200,
+                          rankKey: 'Q1'),
+                      new CircularSegmentEntry(medium, Colors.amberAccent.shade200,
+                          rankKey: 'Q2'),
+                      new CircularSegmentEntry(high, Colors.redAccent.shade200,
+                          rankKey: 'Q3'),
+                    ],
+                    rankKey: 'Quarterly Profits',
+                  ),
+                ];
+                _chartKey.currentState.updateData(nextData);
+                return CustomScrollView(
+                  slivers: <Widget>[
+                    TopWidget(_chartKey, snapshot),
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate((context, index) {
+                        final todo =
+                            Store.fromSnapshot(snapshot.data.documents[index]);
+                        return GestureDetector(
+                            child: Slidable(
+                              actionPane: SlidableDrawerActionPane(),
+                              actionExtentRatio: 0.25,
+                              child: TodoWidget(todo),
+                              actions: <Widget>[
+                                IconSlideAction(
+                                  caption: 'Share',
+                                  color: Colors.indigo,
+                                  icon: Icons.share,
+                                ),
+                              ],
+                              secondaryActions: <Widget>[
+                                IconSlideAction(
+                                  caption: 'Delete',
+                                  color: Colors.red,
+                                  icon: Icons.delete,
+                                  onTap: () => _removeTodoItem(
+                                      snapshot.data.documents[index]),
+                                ),
+                              ],
                             ),
-                          ],
-                          secondaryActions: <Widget>[
-                            IconSlideAction(
-                              caption: 'Delete',
-                              color: Colors.red,
-                              icon: Icons.delete,
-                              onTap: () => _removeTodoItem(snapshot.data.documents[index]),
-                            ),
-                          ],
-                        ),
-                        onTap: () => showDialog(
-                              context: context,
-                              builder: (_) => FunkyOverlay(todo),
-                            ).then((x) => _updateTodo(
-                                snapshot.data.documents[index], x)));
-                  },
-                      childCount: snapshot.hasData
-                          ? snapshot.data.documents.length
-                          : 0),
+                            onTap: () => showDialog(
+                                  context: context,
+                                  builder: (_) => FunkyOverlay(todo),
+                                ).then((x) => _updateTodo(
+                                    snapshot.data.documents[index], x)));
+                      },
+                          childCount: snapshot.hasData
+                              ? snapshot.data.documents.length
+                              : 0),
+                    ),
+                  ],
                 );
-              },
-            )
-          ],
-        )),
+              }),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => _addTodoItem(),
+          backgroundColor: Colors.amber,
           child: Icon(
             Icons.add,
-            color: Colors.yellow,
+            color: Colors.white,
           ),
           foregroundColor: Colors.pink,
         ));
--- lib/widgets/top_widget.dart
+++ lib/widgets/top_widget.dart
@@ -1,7 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:toodoo/models/store_model.dart';
+import 'package:flutter_circular_chart/flutter_circular_chart.dart';
 
 class TopWidget extends StatelessWidget {
+  const TopWidget(this._chartKey, this._snapshot);
+
+  final GlobalKey<AnimatedCircularChartState> _chartKey;
+  final AsyncSnapshot<QuerySnapshot> _snapshot;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +20,40 @@ class TopWidget extends StatelessWidget {
                   color: Colors.amber,
                   child: Align(
                     alignment: Alignment.center,
-                    child: Icon(Icons.star, size: 50),
+                    child:  AnimatedCircularChart(
+                      key: _chartKey,
+                      size: Size.fromRadius(100),
+                      initialChartData: <CircularStackEntry>[
+                        new CircularStackEntry(
+                          <CircularSegmentEntry>[
+                            new CircularSegmentEntry(
+                              33.3,
+                              Colors.greenAccent.shade200,
+                              rankKey: 'low',
+                            ),
+                            new CircularSegmentEntry(
+                              33.3,
+                              Colors.amberAccent.shade200,
+                              rankKey: 'medium',
+                            ),
+                            new CircularSegmentEntry(
+                              33.3,
+                              Colors.redAccent.shade200,
+                              rankKey: 'height',
+                            ),
+                          ],
+                          rankKey: 'progress',
+                        ),
+                      ],
+                      chartType: CircularChartType.Radial,
+                      percentageValues: true,
+                      // holeLabel: priorities.toString(),
+                      labelStyle: new TextStyle(
+                        color: Colors.blueGrey[600],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24.0,
+                      ),
+                    )
                   )),
             );
   }
